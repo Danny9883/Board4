3059a8860dcadb1436bc8106a8b6776cb926c5c4
danny9883 <danny9883@naver.com>
2026-05-04 16:51:35 +0900

    Board4 BoardList Controller 작업중

--- src/main/java/com/green/board/controller/BoardController.java
+++ src/main/java/com/green/board/controller/BoardController.java
@@ -2,21 +2,31 @@ package com.green.board.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.green.board.dto.BoardDto;
+import com.green.board.mapper.BoardMapper;
+import com.green.menus.dto.MenuDTO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/Board")
 public class BoardController {
 	
+	@Autowired
+	private  BoardMapper  boardMapper;
+	
 	// /Board/List
 	@RequestMapping("/List")
-	public  ModelAndView  list() {
+	public  ModelAndView  list( MenuDTO menuDto ) {
 		
-		List<BoardDto> boardList  = boardMapper.getBoardList<>();
+		List<BoardDto> boardList  = boardMapper.getBoardList(menuDto);
+		log.info("boardList: " + boardList);
 		
 		ModelAndView  mv  = new ModelAndView();
 		mv.setViewName("board/list");
